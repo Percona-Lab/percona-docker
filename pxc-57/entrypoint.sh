11f3e52efa6ac269c83bb3506b2acefe01cf2691
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-10 16:35:25 +0100

    Fix "xinetd" service start bug!

--- pxc-57/entrypoint.sh
+++ pxc-57/entrypoint.sh
@@ -156,6 +156,9 @@ set -e
 
 fi
 
+# [elouizbadr] : Launch xinetd service for HAProxy
+/etc/init.d/xinetd start
+
 #--log-error=${DATADIR}error.log
 exec mysqld --user=mysql --wsrep_cluster_name=$CLUSTER_NAME --wsrep_cluster_address="gcomm://$cluster_join" --wsrep_sst_method=xtrabackup-v2 --wsrep_sst_auth="xtrabackup:$XTRABACKUP_PASSWORD" --wsrep_node_address="$ipaddr" $CMDARG
 
